922ede22b2a4885ff78f0254a871011bd742e9f0
Abdallah Ahmed <abdallahar1974@gmail.com>
2020-01-12 03:23:11 +0200

    testing dependencies

---
+++ database/mongo.test.js
@@ -0,0 +1,14 @@
+/**
+ * Connect to a new in-memory database before running any tests.
+ */
+beforeAll(async () => await dbHandler.connect());
+
+/**
+ * Clear all test data after every test.
+ */
+afterEach(async () => await dbHandler.clearDatabase());
+
+/**
+ * Remove and close the db and server.
+ */
+afterAll(async () => await dbHandler.closeDatabase());
