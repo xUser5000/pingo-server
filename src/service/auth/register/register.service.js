ee7c791698fe6878a959d5c455ee347618d77e3a
Abdallah Ahmed <abdallahar1974@gmail.com>
2020-01-13 01:27:37 +0200

    wrote the registration business logic

--- src/service/auth/register/register.service.js
+++ src/service/auth/register/register.service.js
@@ -1,3 +1,13 @@
+const { registerSchema } = require('./register.schema')
+const { validate } = require('../../../util/validator/validator.util')
+const { hash } = require('../../../util/hash/hash.util')
+const { InvalidInputError } = require('../../error/InvalidInputError')
+const { ForbiddenError } = require('../../error/ForbiddenError')
+const {
+    findUserByEmail,
+    findUserByUsername,
+    saveUser
+} = require('../../../database/repository/user.repo')
 
 /**
  * @description Creates a new user account
@@ -7,4 +17,29 @@
  */
 module.exports.register = async req => {
     
+    // validation
+    const result = await validate(req, registerSchema);
+    if (result) {
+        throw new InvalidInputError(result)
+    }
+
+    // look for users with same email address
+    if (await findUserByEmail(req['email'])) {
+        throw new ForbiddenError('Email address is already in use');
+    }
+
+    // look for users with same username
+    if (await findUserByUsername(req['username'])) {
+        throw new ForbiddenError('Username is already in use');
+    }
+
+    // hash the password
+    req['password'] = hash(req.password)
+
+    // add a default bio
+    req['bio'] = 'I am a new Pingo user'
+
+    // save the user document
+    return (await saveUser(req));
+
 }
