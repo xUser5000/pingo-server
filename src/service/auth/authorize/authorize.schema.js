b594960740c25da6c9d9aaf3ff4b33e4dfb74bf5
Abdallah Ahmed <abdallahar1974@gmail.com>
2020-01-24 03:59:11 +0200

    tested the authorizer middleware

--- src/service/auth/authorize/authorize.schema.js
+++ src/service/auth/authorize/authorize.schema.js
@@ -4,6 +4,5 @@ const { buildSchema } = require('../../../util/validator.util')
 module.exports.authorizeSchema = buildSchema({
     token: joi
             .string()
-            .regex(/^[a-zA-Z0-9\-_]+?\.[a-zA-Z0-9\-_]+?\.([a-zA-Z0-9\-_]+)?$/)
             .required()
 })
--- src/web/middleware/tokenAuthorizer.middle.js
+++ src/web/middleware/tokenAuthorizer.middle.js
@@ -2,15 +2,16 @@ const { authorize } = require('../../service/auth/authorize/authorize.service')
 
 const { HttpError } = require('../../service/error/HttpError')
 
-const tokenAuthorizer = (req, res, next) => {
+const tokenAuthorizer = async (req, res, next) => {
 
     const token = req.headers['x-auth-token']
 
     try {
-        const uid = authorize(token)
+        const uid = await authorize(token)
         req.uid = uid
         next()
     } catch (e) {
+        /* istanbul ignore else */
         if (e instanceof HttpError) res.status(e.code).send({ errors: [e.message] })
         else res.status(500).send({ errors: ['Internal server error'] })
     }
